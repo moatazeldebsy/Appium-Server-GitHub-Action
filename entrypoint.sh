b0f8c085a8a2ba4b5c805966746161b3787c2717
Dimitri Kopriwa <77674046+kopax-polyconseil@users.noreply.github.com>
2022-10-31 11:35:01 +0200

    set appium to 2.x, 1.x support is finish

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@ set -ex
 
 echo "Install and Running Appium Server as a Background process"
 
-sudo npm install -g appium --unsafe-perm=true --allow-root
+sudo npm install -g appium@next --unsafe-perm=true --allow-root
 appium -v
 appium &>/dev/null &

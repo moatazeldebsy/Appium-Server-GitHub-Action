ae14fcf8fb95de7ed16815c1bc99fa476b4cc784
Moataz Nabil <45570191+moatazeldebsy@users.noreply.github.com>
2022-11-04 19:44:00 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,3 +6,11 @@ echo "Install and Running Appium Server as a Background process"
 sudo npm install -g appium --unsafe-perm=true --allow-root
 appium -v
 appium &>/dev/null &
+
+sudo npm install -g appium@next
+appium driver install xcuitest
+appium driver install espresso
+appium driver install flutter
+
+appium -v
+appium --log appium.log &>/dev/null &
